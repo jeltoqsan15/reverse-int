e4c7b8328c2c9cca63d031d373b0fe7c65174b07
jeltoqsan15 <realrss@mail.ru>
2023-01-09 20:17:22 +0500

    feat: add code to index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function reverse (n) {
+  if (n < 0) n = -n;
   
+  let result = 0;
+
+  while (n >= 1) {
+    result = result * 10 + n % 10;
+    n = Math.floor(n / 10);
+  }
+
+  return result;
 }
